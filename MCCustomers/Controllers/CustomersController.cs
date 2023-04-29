b02ea509b2965df238cdf38698b167968cf353f3
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-29 17:46:09 +0200

    MCCustomers-feat: getcustomer endpoint finish

---
+++ MCCustomers/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using MCCustomers.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MCCustomers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly MccustomersContext Database;
+
+        public CustomersController(MccustomersContext _database)
+        {
+            this.Database = _database;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            try
+            {
+                if (Database.Customers is null)
+                {
+                    return NotFound();
+                }
+
+                Customer? customer = await Database.Customers
+                                       .Select(x => x)
+                                       .Where(x => x.Id == id)
+                                       .FirstOrDefaultAsync();
+
+                return Ok(customer);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
--- MCCustomers/Program.cs
+++ MCCustomers/Program.cs
@@ -1,3 +1,5 @@
+using MCCustomers.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<MccustomersContext>();
+
 
 var app = builder.Build();
 
