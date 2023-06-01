0675dad4f407bd33db0d96288fdb873d926fb39b
Domenico Tenace <domenicotenace99@gmail.com>
2023-06-01 15:48:18 +0200

    MCProducts-feat: scaffolding entity models from db

---
+++ MCProducts/Models/McproductsContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MCProducts.Models;
+
+public partial class McproductsContext : DbContext
+{
+    public McproductsContext()
+    {
+    }
+
+    public McproductsContext(DbContextOptions<McproductsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-J1SGJBI5\\SQLEXPRESS;Database=MCProducts;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Price).HasColumnType("decimal(9, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MCProducts/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MCProducts.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string? Description { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public string? Image { get; set; }
+
+    public int? Quantity { get; set; }
+}
